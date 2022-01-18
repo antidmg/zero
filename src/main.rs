4a7c1917a1c23692599c13a79fb36f63813e47bc
Gavin Chan <gavinwhchan@gmail.com>
2022-01-17 22:35:39 -0600

    fix merge conflicts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-<<<<<<< Updated upstream
-fn main() {
-    println!("Hello, world!");
-=======
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() {
     run().await
->>>>>>> Stashed changes
 }
